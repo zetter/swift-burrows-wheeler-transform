03dda32d2133327a2547d72fa8e9367865dd3d64
Zetter <zetter@gmail.com>
2014-08-11 22:03:13 +0100

    Re-write to avoid converting strings to arrays and arrays to strings.

--- bwt.swift
+++ bwt.swift
@@ -13,14 +13,14 @@ class BWT {
       rotatedStrings.append(rotate(stringWithTerminator, count: i))
     }
     sort(&rotatedStrings)
-    var lastChars = rotatedStrings.map {
-      String($0[$0.endIndex.predecessor()])
+    let lastChars = rotatedStrings.map {
+      $0[$0.endIndex.predecessor()]
     }
-    return arrayToString(lastChars)
+    return lastChars.reduce("", combine: +)
   }
 
   func decode(encodedString: String) -> String {
-    let encodedArray = stringToArray(encodedString)
+    let encodedArray = Array(encodedString)
     let n = encodedArray.count
 
     var builtStrings = Array(count: n, repeatedValue: "")
@@ -33,18 +33,6 @@ class BWT {
     }
     let decodedString = builtStrings.filter({ $0.hasSuffix(self.terminator) }).first!
     return decodedString.substringToIndex(decodedString.endIndex.predecessor())
-  }
-
-  func stringToArray(string: String) -> [String] {
-    var array: [String] = []
-    for char in string {
-      array.append(String(char))
-    }
-    return array
-  }
-
-  func arrayToString(array: [String]) -> String {
-    return array.reduce("", combine: +)
   }
 
   func rotate(inputString: String, count: Int) -> String {
